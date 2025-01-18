4ddcc2c41eb951b795446b4c295221269dcf4c3f
RahulKhanSuvo <rahul.khan.suvo@gmail.com>
2025-01-18 14:21:22 +0600

    add user role-based navigation and loading spinner in Home component

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,8 +1,26 @@
+import { useNavigate } from "react-router-dom";
+import useAuth from "../../Hooks/useAuth";
+import useUserStatus from "../../Hooks/useUserStatus";
 import About from "./About";
 import Banner from "./Banner";
 import Packages from "./Packages";
+import LoadingSpinner from "../../Components/LoadingSpinner";
 
 const Home = () => {
+  const { user, loading } = useAuth();
+  const navigate = useNavigate();
+  const { userDetails, isLoading } = useUserStatus();
+  if (isLoading || loading) {
+    return <LoadingSpinner></LoadingSpinner>;
+  }
+  if (user) {
+    if (userDetails?.role === "hr") {
+      return navigate("/hrHome");
+    } else if (userDetails?.role === "employee") {
+      return navigate("/employeeHome");
+    }
+  }
+
   return (
     <div className="lg:container mx-auto">
       <Banner></Banner>
